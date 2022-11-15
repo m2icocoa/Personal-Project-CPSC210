5a112d9dc1c14f423991d69022d29dd8b58a32fa
boba <boba@student.ubc.ca>
2022-11-14 17:31:52 -0800

    Phase 3 GUI of seat selection

---
+++ src/main/ui/gui/Selection.java
@@ -0,0 +1,78 @@
+package ui.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Selection {
+    private static JLabel levelSelected;
+    private static JTextField levelText;
+    private static JLabel sectionSelected;
+    private static JTextField sectionText;
+    private static JLabel rowSelected;
+    private static JTextField rowText;
+    private static JLabel numberSelected;
+    private static JTextField numberText;
+    private static JButton button;
+    private static JLabel success;
+
+    public Selection() {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+
+        frame.setSize(1000, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.add(panel);
+
+        panel.setLayout(null);
+
+        levelSelected = new JLabel("Level: ");
+        levelSelected.setBounds(10,20,80,25);
+        panel.add(levelSelected);
+
+        levelText = new JTextField(20);
+        levelText.setBounds(100,20,165,25);
+        panel.add(levelText);
+
+        sectionSelected = new JLabel("Section: ");
+        sectionSelected.setBounds(10,50,80,25);
+        panel.add(sectionSelected);
+
+        sectionText = new JTextField();
+        sectionText.setBounds(100,50,165,25);
+        panel.add(sectionText);
+
+        rowSelected = new JLabel("Row: ");
+        rowSelected.setBounds(10,80,80,25);
+        panel.add(rowSelected);
+
+        rowText = new JTextField();
+        rowText.setBounds(100,80,165,25);
+        panel.add(rowText);
+
+        numberSelected = new JLabel("Number: ");
+        numberSelected.setBounds(10,110,80,25);
+        panel.add(numberSelected);
+
+        numberText = new JTextField();
+        numberText.setBounds(100,110,165,25);
+        panel.add(numberText);
+
+        button = new JButton("Add");
+        button.setBounds(10,140,80,25);
+        panel.add(button);
+
+        success = new JLabel("");
+        success.setBounds(10,110,300,25);
+        panel.add(success);
+
+        frame.setVisible(true);
+    }
+
+
+    public static void main(String[] args) {
+        new Selection();
+    }
+}
